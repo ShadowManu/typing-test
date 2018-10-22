427e049dbe8cc5666d7fd54f0fe808c8f0a8c33e
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-10-22 04:46:31 -0400

    initial solution commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
 
+import { round, size, words } from 'lodash';
+import { Subscription, interval } from 'rxjs';
+
+const TIMER_RESOLUTION = 100; // milliseconds
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'typing-test';
+  // tslint:disable-next-line:max-line-length
+  sample = 'Lorem ipsum dolor sit amet';
+  value = '';
+  words = 0;
+  time = 0;
+
+  started: boolean;
+  finished: boolean;
+  intervalSub: Subscription;
+
+  onInput(value: string) {
+    // Finishing
+    if (size(value) === size(this.sample)) {
+      this.finished = true;
+      this.intervalSub.unsubscribe();
+      this.intervalSub = undefined;
+
+    }
+
+    // Starting
+    if (this.isStarting(value)) {
+      this.started = true;
+      this.finished = false;
+      this.intervalSub = interval(TIMER_RESOLUTION)
+        .subscribe(num => this.time = num);
+    }
+
+    // Update state
+    this.value = value;
+    this.words = size(words(value));
+  }
+
+  onRestart() {
+    this.started = false;
+    this.finished = false;
+    this.value = '';
+    this.time = 0;
+  }
+
+  /** Utility function to determine if a new value should start the timer */
+  isStarting(value: string) {
+    return (
+      size(this.value) === 0 // Input was empty
+      && size(value) === 1 // Input is a singular character
+      && this.time === 0 // time is fresh
+    );
+  }
+
+  displayWPM() { return this.words / (this.seconds / 60); }
+
+  /** Get timer in seconds */
+  get seconds(): number { return this.time * TIMER_RESOLUTION / 1000; }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatCardModule, MatChipsModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
